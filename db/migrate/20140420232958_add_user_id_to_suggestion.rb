2c0f3387b4e711c4e46ba1264e9db8a8164f944b
Brandonmchu <brandon.mk.chu@gmail.com>
2014-04-22 20:32:33 -0400

    removes unncessessary migrations for deploy

--- db/migrate/20140420232958_add_user_id_to_suggestion.rb
+++ db/migrate/20140420232958_add_user_id_to_suggestion.rb
@@ -2,11 +2,5 @@ class AddUserIdToSuggestion < ActiveRecord::Migration
   def change
     add_column :suggestions, :user_id, :integer
 
-    i = 1
-    Suggestion.all.each do |user|
-    	user.update!(user_id: i)
-    	i += 1
-    end
-
   end
 end
